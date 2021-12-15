dbc6208344ab137652c7978367cb86c66d35221a
Thitipong2442 <71888801+Thitipong2442@users.noreply.github.com>
2021-12-15 11:48:16 +0700

    Add files via upload

---
+++ 15-12-2021/components/AlertExample.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  SafeAreaView,
+  Button,
+} from 'react-native';
+
+const AlertExample = () => {
+
+    const simpleAlertHandler =()=>{
+        alert('Hello I am Simple Alert')
+    }
+    const twoOptionsHandler =()=>{
+        // function to make two option alert
+        Alert.alert(
+            //title
+            'Hello',
+            //body
+            'I am two option alert. Do you want to cancel me ?',
+            [
+                {
+                    text:'Yes',
+                    onPress : ()=>console.log('Yes Pressed')
+                },
+                {
+                    text:'No',
+                    onPress : ()=>console.log('No Pressed')
+                }
+            ],
+            {cancelable:true}
+        )
+    }
+    const threeOptionsHandler =()=>{
+        Alert.alert(
+            //title
+            'Hello',
+            //body
+            'I am three option alert. Do you want to cancel me ?',
+            [
+                {
+                    text:'May be',
+                    onPress : ()=>console.log('Maybe Pressed')
+                },
+                {
+                    text:'Yes',
+                    onPress : ()=>console.log('Yes Pressed')
+                },
+                {
+                    text:'No',
+                    onPress : ()=>console.log('No Pressed')
+                }
+            ],
+            {cancelable:true}
+        )
+    }
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+        <View style={styles.container}>
+            <Button
+            title='Simple Alert'
+            onPress={simpleAlertHandler}
+            />
+            <Button
+            title='Alert with two Options'
+            onPress={twoOptionsHandler}
+            />
+            <Button
+            title='Alert with three Options'
+            onPress={threeOptionsHandler}
+            />
+        </View>
+    </SafeAreaView>
+
+  );
+};
+
+export default AlertExample;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+});
---
+++ 15-12-2021/components/ModelExample.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  Button,
+  SafeAreaView,
+} from 'react-native';
+
+const ModelExample = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={showModal}
+        >
+            <View style={styles.modal}>
+                <Text>Modal is open</Text>
+                <Button
+                title='Click to Close Modal'
+                onPress={()=>setShowModal(!showModal)}
+                />
+            </View>
+        </Modal>
+        {/*Update state to make Modal Visibel*/}
+        <Button
+        title='Click to Open Modal'
+        onPress={()=>setShowModal(!showModal)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ModelExample;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    marginTop: 30,
+  },
+  modal: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#f0fff0',
+    padding: 100,
+  },
+  text: {
+    color: '#3f2949',
+    marginTop: 10,
+  },
+});
