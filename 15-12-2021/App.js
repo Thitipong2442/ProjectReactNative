2647b6746b5623f53cc5b3404479d59eddd4651d
Thitipong2442 <71888801+Thitipong2442@users.noreply.github.com>
2021-12-15 11:46:51 +0700

    Add files via upload

---
+++ 15-12-2021/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+import ModelExample from './components/ModelExample'
+//import AlertExample from './components/AlertExample'
+
+const App = () => {
+  return (
+    
+      //<AlertExample/>
+      <ModelExample/>
+  )
+}
+
+export default App
